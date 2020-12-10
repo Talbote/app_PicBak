4a17878526d3ff82d386c5aa42940ad8a3b8a1fb
Talbote <odolinski@MacBook-Pro-de-odolinski.local>
2020-12-10 18:38:47 +0100

    like icon chang fixed

--- assets/app.js
+++ assets/app.js
@@ -29,37 +29,52 @@ $('.custom-file-input').on('change', function (e) {
 // Function Like Pic
 // récuperation de toute lesclass a js-like
 
-
 function onClickBtnLike(even) { // tu dois recevoir un évenement even en parametre
 
     // event ne bouge pas
     even.preventDefault();
     // le href du liens sur lequelle on click.
-    const spanLogo = this.querySelector('i');
-    const spanCount = this.querySelector('span');
+
+
+    const spanIcon = this.querySelector('svg');
+    const spanCount = this.querySelector('span.js-likes');
     const url = this.href;
-    console.log(spanLogo, url, spanCount);
+
+
 
     // recuperé l'URL avec axios
     // quand il aura une reponse
     // met dans la fonction response ->  console.log(response);
     // response renvois des données -> data qui contient de ce qui est revenu du serveur
     axios.get(url).then(function (response) {
-
         // chercher les likes dans le data
 
         spanCount.textContent = response.data.likes;
 
-        /*   if (spanCount.classList.contains('fas')) {
+        const dataPrefixFas = $('fas-svg');
+        const dataPrefixFar = $('far-svg');
 
-         spanCount.classList.replace('fas', 'far')
-         } else {
-         spanCount.classList.replace('far', 'fas');
-         }
-          */
+        if (spanIcon.classList.contains('far-svg')){
+
+            $('.far-svg').attr('data-prefix','fas')
+
+            spanIcon.classList.replace('far-svg','fas-svg')
+
+        } else {
+
+
+            if (spanIcon.classList.contains('fas-svg')) {
+
+                $('.fas-svg').attr('data-prefix','far')
+
+                spanIcon.classList.replace('fas-svg','far-svg')
+
+            }
+        }
 
     });
 }
+
 
 document.querySelectorAll('a.js-like').forEach(function (link) {
     link.addEventListener('click', onClickBtnLike);
