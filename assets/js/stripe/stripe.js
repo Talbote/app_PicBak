bcc71778cc83a4885b0d61622475c34b5510d04f
Talbote <odolinski@MBPdeodolinski.lan>
2020-12-27 17:03:10 +0100

    work system stripe

--- assets/js/stripe/stripe.js
+++ assets/js/stripe/stripe.js
@@ -1,20 +1,24 @@
-
 var stripe = Stripe('pk_test_51I0mX6L4sACyrZxiVDkU4KLAIZoNNgNwEspC7UZzXjUHhAaeljonSrXt1vbqQ8xWY5z4nA40QD7pqUpVTawNaqbv00KEc97CGJ');
 var checkoutButton = document.getElementById('checkout-button');
 
-checkoutButton.addEventListener('click', function() {
+checkoutButton.addEventListener('click', function () {
     // Create a new Checkout Session using the server-side endpoint you
     // created in step 3.
     fetch('/create-checkout-session', {
         method: 'POST',
     })
-        .then(function(response) {
+
+        .then(function (response) {
             return response.json();
         })
-        .then(function(session) {
-            return stripe.redirectToCheckout({ sessionId: session.id });
+
+        .then(function (session) {
+            return stripe.redirectToCheckout({sessionId: session.id});
+
+
         })
-        .then(function(result) {
+
+        .then(function (result) {
             // If `redirectToCheckout` fails due to a browser or network
             // error, you should display the localized error message to your
             // customer using `error.message`.
@@ -22,7 +26,24 @@ checkoutButton.addEventListener('click', function() {
                 alert(result.error.message);
             }
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.error('Error:', error);
-        });
+        })
+        .then(onSubscriptionComplete);
+
 });
+
+
+function onSubscriptionComplete(session) {
+
+    // Payment was successful.
+    if (session.subscription.status === 'active') {
+
+        alert("Hello! I am an alert box!!");
+        console.log(session.subscription.items.data[0].price.product);
+        // Change your UI to show a success message to your customer.
+        // Call your backend to grant access to your service based on
+
+    }
+}
+
--- src/Controller/PremiumController.php
+++ src/Controller/PremiumController.php
@@ -18,45 +18,44 @@ class PremiumController extends AbstractController
     /**
      * @Route("/create-checkout-session", name="checkout")
      */
-    public function checkoutCreate(): Response
+    public function createSubscription(): Response
     {
 
-           \Stripe\Stripe::setApiKey('sk_test_51I0mX6L4sACyrZxifOb3sy4ExerZ8vd22tkbEDoH0LclFv4cKIfdxEA17vmaMNMx1LX7snYZAVo3A4mDWSBgURdG0013ar2A9E');
+        \Stripe\Stripe::setApiKey('sk_test_51I0mX6L4sACyrZxifOb3sy4ExerZ8vd22tkbEDoH0LclFv4cKIfdxEA17vmaMNMx1LX7snYZAVo3A4mDWSBgURdG0013ar2A9E');
 
-           $checkout_session = \Stripe\Checkout\Session::create([
-               'payment_method_types' => ['card'],
-               'line_items' => [[
-                   'price_data' => [
-                       'currency' => 'eur',
-                       'recurring' => [
-                           'interval' => 'week',
-                       ],
-                       'unit_amount' => 300,
-                       'product_data' => [
-                           'name' => 'PicBak Plus+',
-                           'images' => ["https://i.imgur.com/EHyR2nP.png"],
-                       ],
-                   ],
-                   'quantity' => 1,
-               ]],
-               'mode' => 'subscription',
-                 'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
-                 'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        $checkout_session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => 'eur',
+                    'recurring' => [
+                        'interval' => 'week',
+                    ],
+                    'unit_amount' => 300,
+                    'product_data' => [
+                        'name' => 'PicBak Plus+',
+                        'images' => ["https://i.imgur.com/EHyR2nP.png"],
+                    ],
+                ],
+                'quantity' => 1,
+            ]],
+            'mode' => 'subscription',
+            'success_url' => $this->generateUrl('success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('error', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
 
-           ]);
-
-           return new JsonResponse([
-               'id' => $checkout_session->id,
-           ]);
-
-
+        return new JsonResponse([
+            'id' => $checkout_session->id,
+        ]);
     }
 
     /**
      * @Route("/success/", name="success")
      */
-    public function success(): Response
+    public function successSubscription(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
 
         return $this->render('premium/success.html.twig');
     }
@@ -64,9 +63,87 @@ class PremiumController extends AbstractController
     /**
      * @Route("/error/", name="error")
      */
-    public function error(): Response
+    public function errorSubscription(): Response
     {
 
         return $this->render('premium/error.html.twig');
     }
+
+    /**
+     * @Route("/status/", name="status")
+     */
+    public function statusSubscription(): Response
+    {
+
+        //req_yWe9QR6My88lxB
+        $stripe = new \Stripe\StripeClient("sk_test_51I0mX6L4sACyrZxifOb3sy4ExerZ8vd22tkbEDoH0LclFv4cKIfdxEA17vmaMNMx1LX7snYZAVo3A4mDWSBgURdG0013ar2A9E");
+        $customer = $stripe->customers->create();
+        echo $customer->getLastResponse()->headers["Request-Id"];
+        dd( $stripe->events->all(['limit' => 3]));
+
+        return $this->render('premium/status.html.twig');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * @Route("/cancel-subscription", name="app_cancel_subscription")
+     */
+    public function cancelSubscription(): Response
+    {
+
+        \Stripe\Stripe::setApiKey('sk_test_51I0mX6L4sACyrZxifOb3sy4ExerZ8vd22tkbEDoH0LclFv4cKIfdxEA17vmaMNMx1LX7snYZAVo3A4mDWSBgURdG0013ar2A9E');
+
+        \Stripe\Subscription::update(
+            'sub_IeIZGmw0cNpyxA',
+            [
+                'cancel_at_period_end' => true,
+            ]
+        );
+
+        return $this->render('premium/cancel.html.twig');
+
+    }
+
+    /**
+     * @Route("/reactivating-canceled-subscriptions", name="app_reactivating_canceled_subscription")
+     */
+    public function reactivatingSubscription(): Response
+    {
+
+        \Stripe\Stripe::setApiKey('sk_test_51I0mX6L4sACyrZxifOb3sy4ExerZ8vd22tkbEDoH0LclFv4cKIfdxEA17vmaMNMx1LX7snYZAVo3A4mDWSBgURdG0013ar2A9E');
+
+        $subscription = \Stripe\Subscription::retrieve('sub_IeIZGmw0cNpyxA');
+        \Stripe\Subscription::update('sub_IeIZGmw0cNpyxA', [
+            'cancel_at_period_end' => false,
+            'proration_behavior' => 'create_prorations',
+            'items' => [
+                [
+                    'id' => $subscription->items->data[0]->id,
+                    'price' => 'price_1I2zxtL4sACyrZxiE6cTWgYA',
+                ],
+            ],
+        ]);
+
+        return $this->render('premium/reactivating.html.twig');
+    }
+
+
 }
